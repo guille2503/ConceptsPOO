9d3c0306574babc56b974b0c7d9cdd27f915954c
guille2503 <guiyyo4@gmail.com>
2022-02-27 21:16:21 -0500

    Finish date class

---
+++ conceptsPOO/conceptsPOO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace conceptsPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(new Date(2022, 10, 20));
+                Console.WriteLine(new Date(2022, 11, 20));
+                Console.WriteLine(new Date(2022, 9, 20));
+                Console.WriteLine(new Date(2024, 2, 29));
+            }
+            catch (Exception Error)
+            {
+
+                Console.WriteLine(Error.Message);
+            }
+            Console.WriteLine("Finalizado");
+            Console.ReadKey();
+        }
+
+        
+    }
+}
